62d97c404f4dc67c02aa77cd0eb809dbf1e03ce7
HirosuguTakeshita <takeshita.hirosugu.17@shizuoka.ac.jp>
2020-02-01 18:09:49 +0900

    get just json of only total population from api

--- index.js
+++ index.js
@@ -20,6 +20,29 @@ var checkboxs = new Vue({
                 console.log(error);
             });
     },
-  })
+})
+  
+var trends = new Vue({
+    el: '#trends',
+    data: {
+        datalists: [],
+    },
+    mounted () {
+        axios
+        .get("https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear?prefCode=11", {
+            headers: {
+                "Content-Type": "application/json",
+                "X-API-KEY": "NNdLp1xdZ5PtMo0DwVYkZqUYHAHH7meoecAtqi6X"
+            },
+            data: {},
+       })
+            .then(response => {
+                this.datalists = response.data.result.data;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    },
+})
 
 
